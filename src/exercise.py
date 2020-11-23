3b3579095c3a5b7e6dc2af97e688a3cbd39a606a
rhysrawnson2101 <Rhysrawnson@gmail.com>
2020-11-23 15:12:44 +0000

    simple calculator

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,10 @@
 def main():
     #write your code below this line
-
+  number_one = int(input('please choose your first number:'))
+  number_two = int(input('please cohose your second number:'))
+  print(number_one + number_two)
+  print(number_one - number_two)
+  print(number_one * number_two)
+  print(number_one / number_two)
 if __name__ == '__main__':
     main()
